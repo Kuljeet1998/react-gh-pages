49aff1745bfdb29b926b3b04541f84dd83b9406a
Kuljeet1998 <kbhengura@gmail.com>
2024-04-22 23:19:02 -0400

    Minor changes for the day

---
+++ src/components/tech/tech.js
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import '../work/work.css'
+import './tech.css'
+
+function Tech () {
+   return (
+      <div>
+         <h1>Favorite Tech</h1>
+      </div>
+   )
+}
+
+export default Tech;
