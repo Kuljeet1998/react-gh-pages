ce4a0336635ee3d9a1233bdc575874172c9ad30a
Kuljeet1998 <kbhengura@gmail.com>
2024-04-22 21:53:56 -0400

    Added component work exp

---
+++ src/components/work/work.js
@@ -0,0 +1,190 @@
+import React, { FC } from 'react';
+import '../education/education.css'
+import './work.css'
+import '../../App.css'
+
+function Work () {
+   return (
+      <div>
+         <table>
+            <tr>
+               <th className='section'>WORK</th>
+               <th className='arrange'>George Mason University</th>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='pursue arrange'>Graduate Teaching Assistant  • <span className='graduation'>Jan 2023 – May 2024 </span></td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Course IT461: Web Development in Cloud - Hands-on AWS, caching with ElastiCache, NoSQL, CI/CD</td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Course IT431: Advanced Web Development - Teach & Grade on topics - ReactJs, SPAs, REST APIs, Cloud deployment</td>
+            </tr>
+            
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Course IT293: Junior Transition - Make students job ready</td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange'><a href='https://catalog.gmu.edu/courses/it/' target="_blank">View course catalog</a></td>
+            </tr>
+            
+            <tr>
+               <td></td><td></td>
+            </tr><br></br><br></br>
+
+
+
+            <tr>
+               <th></th>
+               <th className='arrange'>GetSetUp</th>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='pursue arrange'>Software Development Engineer • <span className='graduation'>July 2021 – June 2022</span></td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Engineered a framework using 
+                  <span className='font-important'> ReactJs, NodeJs</span> to build a scalable platform 
+                  for older adults to participate in and host classes handling up to 
+                  100,000 users in a single video class.
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Developed e-mailing 
+                  <span className='font-important'> microservice</span> to notify user class 
+                  timings and booking info using 
+                  <span className='font-important'> AWS: Lambda, SNS & SQS.</span>
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+                  <td className='arrange sub-point'>• Categorized and organized data improving data 
+                  extraction by <span className='font-important'>60% </span> 
+                  leveraging <span className='font-important'>Redis</span> for caching.
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Built APIs using 
+                  <span className='font-important'> Django</span> to reduce the average 
+                  turnaround time by <span className='font-important'>80%</span> (10 to 2 mins) - Analysed data with the 
+                  help of <span className='font-important'>django-silk</span>
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Built APIs using 
+                  Managed the database flow on Amazon <span className='font-important'>RDS </span> 
+                  and connected it with the projects.
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange'><a href='https://www.getsetup.io/' target="_blank">Visit website</a></td>
+            </tr><br></br><br></br>
+            
+
+
+
+            <tr>
+               <th></th>
+               <th className='arrange'>Zaya Learning Labs</th>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='pursue arrange'>Software Development Engineer • <span className='graduation'>Aug 2020 – July 2021</span></td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Built APIs using 
+                  Developed FABLe and BeSomeone projects, managing architecture design and flow 
+                  for web and mobile apps. Utilized <span className='font-important'>Django (Python)</span> for backend development, 
+                  contributing to a total revenue of $750k.
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Overhauled the Sensei project by designing its backend with
+                  <span className='font-important'> Django (Python)</span> and <span className='font-important'>postgresql</span> resulting 
+                  in a <span className='font-important'>67%</span> increase in user traffic.
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Utilized
+                  <span className='font-important'> AWS</span> to deploy projects and their assets by creating new 
+                  <span className='font-important'> EC2</span> instances 
+                  and an <span className='font-important'> S3</span> bucket to store the assets 
+                  (images, videos, documents, etc).
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Managed the database flow on Amazon 
+                  <span className='font-important'> RDS</span> and connected it with the projects.
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange'><a href='https://www.zaya.org/' target="_blank">Visit website</a></td>
+            </tr><br></br><br></br>
+
+
+
+            <tr>
+               <th></th>
+               <th className='arrange'>Zaya Learning Labs</th>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='pursue arrange'>Intern • <span className='graduation'>Jan 2020 – Feb 2021</span></td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Developed a web application as a component of 
+               Zaya's Incident and Production 
+               support system, improving user experience and driving downloads to 
+               15,000 on the launch day. Implemented using  
+               <span className='font-important'> Django (Python)</span>.
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange'><a href='https://www.zaya.org/' target="_blank">Visit website</a></td>
+            </tr><br></br><br></br>
+
+
+            <tr>
+               <th></th>
+               <th className='arrange'>Brainvire </th>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='pursue arrange'>Intern • <span className='graduation'>Dec 2018 – Jan 2019</span></td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange sub-point'>• Crafted web-pages for SAP using 
+               <span className='font-important'>  .Net (C#)</span> for backend and
+               <span className='font-important'>  Bootstap (HTML,CSS)</span> for 
+               <span className='font-important'>  frontend</span> - increase revenue by 
+               <span className='font-important'>  45%</span>.
+               </td>
+            </tr>
+            <tr>
+               <td></td>
+               <td className='arrange'><a href='https://www.brainvire.com/' target="_blank">Visit website</a></td>
+            </tr><br></br><br></br>
+         </table>
+      </div>
+   )
+}
+
+export default Work;
